a696f6abbedcb26d6688a9b5785714e0cb6b0b37
nroumiantzeff <nicolas.roumiantzeff@gmail.com>
2016-08-16 16:48:48 +0200

    Update ch3rds.js
    add actuall sound

--- ch3rds/ch3rds.js
+++ ch3rds/ch3rds.js
@@ -1,4 +1,7 @@
-function Harmonic(container, id, label){
+function Harmonic(audioContext, master, container, id, label){
+	this.audioContext = audioContext;
+	this.master = master;
+	this.oscillator = new Oscillator(audioContext, master);
 	this.container = container;
 	this.id = id;
 	this.label = label;
@@ -11,16 +14,21 @@ function Harmonic(container, id, label){
 	else{
 		this.div.className = "harmonic";
 	}
+	if (this.on){
+		this.oscillator.setGain(1);
+	}
 	this.div.id = id;
 	this.div.appendChild(document.createTextNode(label));
 	container.appendChild(this.div);
 	var harmonic = this;
 	this.div.onmousedown = function(){
 		if (harmonic.on){
+			harmonic.oscillator.setGain(0);
 			harmonic.on = false;
 			harmonic.div.className = "harmonic";
 		}
 		else{
+			harmonic.oscillator.setGain(1);
 			harmonic.on = true;
 			harmonic.div.className = "harmonic harmonic-on";
 		}
@@ -28,17 +36,22 @@ function Harmonic(container, id, label){
 	this.div.addEventListener('touchstart', function (event){
 		event.preventDefault();
 		if (harmonic.on){
+			harmonic.oscillator.setGain(0);
 			harmonic.on = false;
 			harmonic.div.className = "harmonic";
 		}
 		else{
+			harmonic.oscillator.setGain(1);
 			harmonic.on = true;
 			harmonic.div.className = "harmonic harmonic-on";
 		}
 	}, false);
 }
 
-function Chord(container, id, label, type){
+function Chord(audioContext, master, container, id, label, type){
+	this.audioContext = audioContext;
+	this.master = master;
+	this.gain = new Gain(audioContext, master);
 	this.container = container;
 	this.id = id;
 	this.label = label;
@@ -50,7 +63,7 @@ function Chord(container, id, label, type){
 	var HARMONICS = ["root", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th"];
 	var harmonics = [];
 	for (var index = HARMONICS.length - 1; index >= 0; index--){
-		var harmonic = new Harmonic(this.whole, this.id + "." + index, HARMONICS[index]);
+		var harmonic = new Harmonic(audioContext, this.gain, this.whole, this.id + "." + index, HARMONICS[index]);
 		harmonics.push(harmonic);
 	}
 	this.div = document.createElement("DIV");
@@ -60,28 +73,40 @@ function Chord(container, id, label, type){
 	this.whole.appendChild(this.div);
 	var chord = this;
 	this.div.onmousedown = function(){
+		chord.gain.setGain(1);
 		chord.on = true;
 		chord.div.className = "chord-button chord-on";
 	};
 	this.div.onmouseup = function(){
+		chord.gain.setGain(0);
 		chord.on = false;
 		chord.div.className = "chord-button";
 	};
 	this.div.addEventListener('touchstart', function (event){
 		event.preventDefault();
+		chord.gain.setGain(1);
 		chord.on = true;
 		chord.div.className = "chord-button chord-on";
 	}, false);
 	this.div.addEventListener('touchend', function (event){
 		event.preventDefault();
+		chord.gain.setGain(0);
 		chord.on = false;
 		chord.div.className = "chord-button";
 	}, false);
 }
 
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext();
+var master = new Gain(audioContext);
+master.setGain(0.1);
+
 var chords = [];
-chords.push(new Chord(document.body, "chord1", "F#m", "minor"));
-chords.push(new Chord(document.body, "chord2", "A", "major"));
-chords.push(new Chord(document.body, "chord2", "G", "major"));
-chords.push(new Chord(document.body, "chord2", "Dm", "major"));
-chords.push(new Chord(document.body, "chord2", "Bm", "major"));
+chords.push(new Chord(audioContext, master, document.body, "chord1", "F#m", "minor"));
+//debug chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "major"));
+//debug chords.push(new Chord(audioContext, master, document.body, "chord2", "G", "major"));
+//debug chords.push(new Chord(audioContext, master, document.body, "chord2", "Dm", "major"));
+//debug chords.push(new Chord(audioContext, master, document.body, "chord2", "Bm", "major"));
+
+
+//debug var debug = new Harmonic(audioContext, master, document.body, "debug", "debug");//debug
