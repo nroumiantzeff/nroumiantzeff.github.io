1c9ee4ab69a6a79448c08165e7654773c5b134df
nroumiantzeff <nicolas.roumiantzeff@gmail.com>
2016-08-17 13:46:22 +0200

    Update ch3rds.js
    differenciate tones

--- ch3rds/ch3rds.js
+++ ch3rds/ch3rds.js
@@ -1,25 +1,30 @@
-function Harmonic(audioContext, master, container, id, label){
+function Harmonic(audioContext, master, container, id, tone, type, order){
 	this.audioContext = audioContext;
 	this.master = master;
-	this.oscillator = new Oscillator(audioContext, master);
 	this.container = container;
 	this.id = id;
-	this.label = label;
+	this.tone = tone;
+	this.type = type;
+	this.order = order;
+	this.frequency = this.getFrequency(tone, type, order);
+	this.detune = this.getDetune(tone, type, order);
+	this.label = this.getLabel(type, order);
+	this.oscillator = new Oscillator(audioContext, master);
+	this.oscillator.setFrequency(this.frequency);
+	this.oscillator.setDetune(this.detune);
 	this.on = false;
-	this.div = document.createElement("DIV");
-	if (label == "root" || label == "3rd" || label == "5th"){
+	if (order == 1 || order == 3 || order == 5){
 		this.on = true;
-		this.div.className = "harmonic harmonic-on";
 	}
-	else{
-		this.div.className = "harmonic";
-	}
+	this.div = document.createElement("DIV");
+	this.div.className = "harmonic";
+	this.div.id = id;
+	this.div.appendChild(document.createTextNode(this.label));
+	container.appendChild(this.div);
 	if (this.on){
+		this.div.className = "harmonic harmonic-on";
 		this.oscillator.setGain(1);
 	}
-	this.div.id = id;
-	this.div.appendChild(document.createTextNode(label));
-	container.appendChild(this.div);
 	var harmonic = this;
 	this.div.onmousedown = function(){
 		if (harmonic.on){
@@ -48,28 +53,56 @@ function Harmonic(audioContext, master, container, id, label){
 	}, false);
 }
 
-function Chord(audioContext, master, container, id, label, type){
+Harmonic.prototype.getFrequency = function(tone, type, order){
+	return 440;
+}
+
+Harmonic.prototype.getDetune = function(tone, type, order){
+	var detune = 0;
+	var TONES = { "A": 0, "A#": 100, "B": 200, "C": 300, "C#": 400, "D": 500, "D#": 600, "E": 700, "F": 800, "F#": 900, "G": 1000, "G#": 1100};
+	detune += TONES[tone];
+	var pitch = (order - 1) % 7;
+	var octave = (order - 1 - pitch) / 7;
+	if (type === "minor"){
+		var MINOR = [0, 200, 300, 500, 700, 800, 1000];
+		detune += MINOR[pitch];
+	}
+	else{
+		var MAJOR = [0, 200, 400, 500, 700, 900, 1100];
+		detune += MAJOR[pitch];
+	}
+	detune += octave * 1200;
+	return detune;
+}
+
+Harmonic.prototype.getLabel = function(type, order){
+	var label = (order === 1)? "1st": (order === 2)? "2nd": (order === 3)? "3rd": order + "th";
+	return label;
+}
+
+function Chord(audioContext, master, container, id, tone, type){
 	this.audioContext = audioContext;
 	this.master = master;
 	this.gain = new Gain(audioContext, master);
 	this.container = container;
 	this.id = id;
-	this.label = label;
+	this.tone = tone;
 	this.type = type;
+	this.label = this.getLabel(tone, type);
 	this.on = false;
 	this.whole = document.createElement("DIV");
 	this.whole.className = "chord";
 	container.appendChild(this.whole);
 	var HARMONICS = ["root", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th"];
 	var harmonics = [];
-	for (var index = HARMONICS.length - 1; index >= 0; index--){
-		var harmonic = new Harmonic(audioContext, this.gain, this.whole, this.id + "." + index, HARMONICS[index]);
+	for (var order = 17; order > 0; order--){
+		var harmonic = new Harmonic(audioContext, this.gain, this.whole, this.id + "." + order, tone, type, order);
 		harmonics.push(harmonic);
 	}
 	this.div = document.createElement("DIV");
 	this.div.className = "chord-button";
 	this.div.id = id;
-	this.div.appendChild(document.createTextNode(label));
+	this.div.appendChild(document.createTextNode(this.label));
 	this.whole.appendChild(this.div);
 	var chord = this;
 	this.div.onmousedown = function(){
@@ -96,17 +129,31 @@ function Chord(audioContext, master, container, id, label, type){
 	}, false);
 }
 
+Chord.prototype.getLabel = function(tone, type){
+	var label = tone;
+	if (type === "minor"){
+		label += "m";
+	}
+	return label;
+}
+
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext = new AudioContext();
 var master = new Gain(audioContext);
 master.setGain(0.1);
 
 var chords = [];
-chords.push(new Chord(audioContext, master, document.body, "chord1", "F#m", "minor"));
-//debug chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "major"));
-//debug chords.push(new Chord(audioContext, master, document.body, "chord2", "G", "major"));
-//debug chords.push(new Chord(audioContext, master, document.body, "chord2", "Dm", "major"));
-//debug chords.push(new Chord(audioContext, master, document.body, "chord2", "Bm", "major"));
-
-
-//debug var debug = new Harmonic(audioContext, master, document.body, "debug", "debug");//debug
+//debug chords.push(new Chord(audioContext, master, document.body, "chords", "C", "major"));
+//debug chords.push(new Chord(audioContext, master, document.body, "chord1", "A", "major"));
+chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "minor"));
+chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "minor"));
+chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "minor"));
+chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "minor"));
+chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "minor"));
+chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "minor"));
+chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "minor"));
+chords.push(new Chord(audioContext, master, document.body, "chord2", "A", "minor"));
+//chords.push(new Chord(audioContext, master, document.body, "chord2", "F#", "minor"));
+//debug chords.push(new Chord(audioContext, master, document.body, "chord3", "G", "major"));
+//debug chords.push(new Chord(audioContext, master, document.body, "chord4", "D", "minor"));
+//debug chords.push(new Chord(audioContext, master, document.body, "chord5", "B", "minor"));
